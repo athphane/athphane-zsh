dee26ca3985eba07693bb928946b49b37e7a3532
athphane <athphane@gmail.com>
2024-02-26 13:10:27 +0500

    git clone helper command

--- git.zsh
+++ git.zsh
@@ -4,3 +4,20 @@ function wip {
     git commit -m "WIP"
     git push
 }
+
+# 
+function gitCloneHelper {
+    if [ "$#" -eq  "0" ]
+    then
+        cecho red "Please provide a repo name"
+    else
+        case $1 in 
+            *)
+            git clone git@github.com:$1.git
+            ;;
+        esac
+    fi
+}
+
+# Main function alias
+alias gc=gitCloneHelper
