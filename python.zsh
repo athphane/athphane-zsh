5ab58a082a6b0209c4c233aa9cbb28cd8e60070d
athphane <athphane@gmail.com>
2024-12-23 11:15:56 +0500

    add python upgrade command

--- python.zsh
+++ python.zsh
@@ -1,2 +1,5 @@
 # Python Aliases  
 alias python-venv="source venv/bin/activate"
+
+# Upgrade all python packages in the venv
+alias python-upgrade="pip freeze --local | cut -d '=' -f 1 | xargs -n1 pip install -U"
